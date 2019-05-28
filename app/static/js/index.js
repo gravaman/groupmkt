dc35f14e30ff1d644a084767b2492a646b6ad606
fleish <cfleish@gmail.com>
2019-05-28 11:49:27 -0500

    moves frame logic to separate function

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,21 +1,25 @@
 import { lineChart } from '/js/lineChart.js'
 
 $(document).ready(function() {
-	// main visual setup
-	let frame = {
-		width: Math.min($('#main-display-visual').parent().width(), 900),
-	}
-	frame.height = frame.width*$(window).height()/$(window).width()
-	lineChart("#main-display-visual", frame, "data/linechart_data.csv")
+	// main visual
+	lineChart("#main-display-visual", getFrame(), "data/linechart_data.csv")
 
-	// click handlers
+	// sidebar toggle
 	$('#sidebarCollapse').on('click', function() {
 		$('#sidebar').addClass('inactive')
 		$('.overlay').addClass('active')
 	})
+
 	$('#dismiss, .overlay').on('click', function() {
 		$('#sidebar').removeClass('inactive')
 		$('.overlay').removeClass('active')
 	})
 })
 
+function getFrame() {
+	let frame = {
+		width: Math.min($('#main-display-visual').parent().width(), 90)
+	}
+	frame.height = frame.width*$(window).height()/$(window).width()
+	return frame
+}
